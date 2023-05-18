7a13a9beac13482f57412f1c33d294ae6b36e5c0
newfotune <chiemeziemnwoke@yahoo.com>
2023-05-17 20:18:31 -0700

    querries to get, update and create a user

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -24,17 +24,62 @@ func (r *Repository) SaveTokenSignOn(email, token, userType string) error {
 	return nil
 }
 
+func (r *Repository) GetUser(user_id string) (*User, error) {
+	row := r.db.QueryRow(`SELECT id, email, created_at, user_type, email_verified, access_token, refresh_token, expiration_time FROM users where id = $1`, user_id)
+	var id, email, userType, accessToken, refreshToken sql.NullString
+	var createdAt, expirationTime sql.NullTime
+	var emailVerified sql.NullBool
+	err := row.Scan(&id, &email, &createdAt, &userType, &emailVerified, &accessToken, &refreshToken, &expirationTime)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:            id.String,
+		Email:         email.String,
+		EmailVerified: emailVerified.Bool,
+		AccessToken:   accessToken.String,
+		RefreshToken:  refreshToken.String,
+		CreatedAt:     createdAt.Time,
+		Type:          userType.String,
+	}, nil
+}
+
+func (r *Repository) CreateUser(u User) error {
+	_, err := r.db.Exec(
+		`INSERT INTO users (id, email, created_at, user_type, email_verified, access_token, refresh_token, expiration_time) 
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+		u.ID, u.Email, u.CreatedAt, u.Type, u.EmailVerified, u.AccessToken, u.RefreshToken, u.ExpirationTime)
+	return err
+}
+
+func (r *Repository) UpdateAccessToken(userId, accessToken string) error {
+	_, err := r.db.Exec(`UPDATE users SET access_token = $1 WHERE id = $2`, accessToken, userId)
+	return err
+}
+
+func (r *Repository) UpdateRefreshToken(userId, refreshToken string) error {
+	_, err := r.db.Exec(`UPDATE users SET refresh_token = $1 WHERE id = $2`, refreshToken, userId)
+	return err
+}
+
 // GetOrCreateUserFromToken creates or get existing user given a token
 // returns the user struct, whether the user existed already and an error
 func (r *Repository) GetOrCreateUserFromToken(token string) (User, bool, error) {
 	u := User{}
-	row := r.db.QueryRow(`SELECT t.token, t.email, u.id, u.email, u.created_at, t.user_type FROM user_sign_on_token t LEFT JOIN users u ON t.email = u.email WHERE t.token = $1`, token)
-	var tokenRes, id, email, tokenEmail, userType sql.NullString
-	var createdAt sql.NullTime
-	if err := row.Scan(&tokenRes, &tokenEmail, &id, &email, &createdAt, &userType); err != nil {
+	row := r.db.QueryRow(`SELECT id, email, created_at, user_type, email_verified, access_token, expiration_time FROM users where token = $1`, token)
+	//row := r.db.QueryRow(`SELECT t.token, t.email, u.id, u.email, u.created_at, t.user_type
+	// FROM user_sign_on_token t LEFT JOIN users u ON t.email = u.email WHERE t.token = $1`, token)
+	var id, email, userType, accessToken sql.NullString
+	var createdAt, expirationTime sql.NullTime
+	var emailVerified sql.NullBool
+	if err := row.Scan(&id, &email, &createdAt, &userType, &emailVerified, &accessToken, &expirationTime); err != nil {
 		return u, false, err
 	}
-	if !tokenRes.Valid {
+	if !accessToken.Valid {
 		return u, false, errors.New("token not found")
 	}
 	if !email.Valid {
@@ -44,7 +89,7 @@ func (r *Repository) GetOrCreateUserFromToken(token string) (User, bool, error) 
 			return u, false, err
 		}
 		u.ID = userID.String()
-		u.Email = tokenEmail.String
+		u.Email = accessToken.String
 		u.CreatedAt = time.Now()
 		u.Type = userType.String
 		u.CreatedAtHumanised = humanize.Time(u.CreatedAt.UTC())
